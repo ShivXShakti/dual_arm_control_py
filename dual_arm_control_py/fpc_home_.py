130fb78dd1ce8bf7638ce34509738729fcc3cf9e
cstar <kuldeeplakhansons@gmail.com>
2025-06-06 17:23:34 +0530

    updated

---
+++ dual_arm_control_py/fpc_home_.py
@@ -0,0 +1,69 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64MultiArray
+import math
+from dual_arm_control_py.home_traj_generator import TrajectoryGenerator
+#from dual_arm_control_py.traj_generator_ik import TrajectoryGenerator
+from sensor_msgs.msg import JointState
+import numpy as np
+import pandas as pd
+
+class PositionCommander(Node):
+    def __init__(self):
+        super().__init__('position_commander')
+        self.sampling_frequency = 100
+        self.publisher_ = self.create_publisher(Float64MultiArray, '/position_controller/commands', 10)
+        #self.publisher_ = self.create_publisher(Float64MultiArray, '/forward_position_controller/commands', 10)
+    
+        self.timer = self.create_timer(1/self.sampling_frequency, self.timer_callback)  # 100 Hz
+        self.start_time = self.get_clock().now().nanoseconds
+        self.obj = TrajectoryGenerator()
+        self.subscription = self.create_subscription(
+            JointState,
+            '/joint_states',
+            self.listener_callback,
+            10
+        )
+
+        self.num_joints = 16  # Adjust according to your robot
+        self.js = None
+        self.joint_names = [
+            'J1_left', 'J2_left', 'J3_left', 'J4_left', 'J5_left', 'J6_left', 'L_link7_to_flange',
+            'J1_right', 'J2_right', 'J3_right', 'J4_right', 'J5_right', 'J6_right', 'R_link7_to_flange',
+            'neck_joint', 'head_joint'
+        ]
+        self.ordered_positions = None #[0.0 for _ in range(len(self.joint_names))]
+        self.t_counter = 0.0
+        self.ct = 0
+        self.js_callback = False
+        self.T_final = np.array([[0, -1, 0, 0], 
+                                 [0, 0, -1, -.10555-0.176-0.3-0.32-0.225], #0.10555,0.176,0.3,0.32,0.2251
+                                 [1, 0, 0, 0], 
+                                 [0, 0, 0, 1]])
+        self.T_init = None
+
+    def listener_callback(self, msg):
+        name_to_position = dict(zip(msg.name, msg.position))
+        self.ordered_positions = [name_to_position.get(self.joint_names[i], float('nan')) for i in range(len(self.joint_names))]
+        if not self.js_callback:
+            _, self.T_init = self.obj.FK(self.ordered_positions[:7], self.obj.dh_l)
+        self.js_callback = True
+    def timer_callback(self):
+        if self.ordered_positions is None:
+            return
+        msg = Float64MultiArray()
+        
+        joints = self.obj.get_joints(self.t_counter, [self.ordered_positions[7], self.ordered_positions[8], self.ordered_positions[9], self.ordered_positions[10], self.ordered_positions[11], self.ordered_positions[12], self.ordered_positions[13]], self.obj.dh_l, self.T_init, self.T_final)
+        msg.data = [joints[0], joints[1], joints[2], joints[3], joints[4], joints[5], joints[6], joints[7], joints[8], joints[9], joints[10], joints[11], joints[12], joints[13], 0.0, 0.0]
+        self.t_counter += 1/self.sampling_frequency
+        self.publisher_.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PositionCommander()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- dual_arm_control_py/online_joints_trajectory_generator.py
+++ dual_arm_control_py/online_joints_trajectory_generator.py
@@ -11,77 +11,33 @@ import pandas as pd
 
 
 class TrajectoryGenerator():
-    def __init__(self, T_init = None, T_final=None, sampling_frequency=100, traj_time = 20, traj_type="straight", dh_l=[0.10555,0.176,0.3,0.32,0.2251]):
+    def __init__(self, sampling_frequency=100, traj_time = 20, dh_l=[0.10555,0.176,0.3,0.32,0.2251]):
         self.traj_time = traj_time
-        self.traj_type = traj_type
         self.traj_status = True
         self.dh_l = dh_l
         self.sampling_frequency = sampling_frequency
-        l1,l2, l3, l4, l5 = self.dh_l[0],self.dh_l[1],self.dh_l[2],self.dh_l[3],self.dh_l[4]
-
-        ##### trajectory flags
+        #------- trajectory execution flags -------
         self.grip_reach_f = False
         self.put_reach_f = False
-
-        #### null space
+        #-------- null space constraints----------
         self.max_joints_limit = np.array([3.14,1.56,3.141,2.446,3.141,1.306,6.28,3.14,1.56,3.141,2.446,3.141,1.306,6.28])
         self.min_joints_limit = np.array([-3.141,-1.56,-3.141,-1.365,-3.141,-1.306,-6.28,-3.141,-1.56,-3.141,-1.365,-3.141,-1.306,-6.28])
         self.mean_joints_limit = (self.max_joints_limit + self.min_joints_limit)/2
         self.max_min_dif_sq = (self.max_joints_limit-self.min_joints_limit)**2
         self.null_space = True
-
-        self.T_init = np.array([[0, -1, 0, 0], 
-                                 [0, 0, -1, -l1-l2-l3-l4-l5], 
-                                 [1, 0, 0, 0], 
-                                 [0, 0, 0, 1]]) if T_init is None else T_init
-        
-        ## tested
-        self.T_final = np.array([[0, 0, 1, l4+l5], 
-                                 [0, -1, 0, -0.4], 
-                                 [1, 0, 0, -l3], 
-                                 [0, 0, 0, 1]]) if T_final is None else T_final
-        #str working
-        """self.T_final = np.array([[0, -1, 0, 0], 
-                                 [-1, 0, 0, -0.5], 
-                                 [0, 0, -1, -l3-l4], 
-                                 [0, 0, 0, 1]]) if T_final is None else T_final"""
-        #### half way st
-        """self.T_final = np.array([[0, -1, 0, 0], 
-                                 [-0.2588, 0, -0.9659, -l1-l2-l3-l4-l5+0.05], 
-                                 [0.9659, 0, -0.2588, -0.1], 
-                                 [0, 0, 0, 1]]) if T_final is None else T_final"""
-        #ee rot
-        """self.T_final = np.array([[1, 0, 0, 0], 
-                                 [0, 0, -1, -l1-l2-l3-l4-l5], 
-                                 [0, 1, 0, 0], 
-                                 [0, 0, 0, 1]]) if T_final is None else T_final"""
+        #------- traj generator --------
         self.R_prev = None
-        ### traj generator
         self.prev_position = None
         self.prev_euler = None
-
-        ### click
+        #-------- click ----------
         self.joint_states = None #[3.14, -1.5699755717780695, 1.57,-1.569931101434043, -1.57, 1.57]
         self.posedot = None
         self.pose = None
         self.pose_a = None
         self.joint_states_prev = None #[3.14,-1.5699755717780695, 1.57,-1.569931101434043, -1.57, 1.57]
         self.joint_angles_both = None
-        
-        """self.k_ = np.array([[7.11e-4,0,0,0,0,0],
-                            [0,0.0016,0,0,0,0],
-                            [0,0,0.0064,0,0,0],
-                            [0,0,0,0.000256,0,0],
-                            [0,0,0,0,0.00256,0],
-                            [0,0,0,0,0,0.00256]])"""
-        self.k_ = np.array([[0.0005,0,0,0,0,0],
-                            [0,0.0011,0,0,0,0],
-                            [0,0,0.0044,0,0,0],
-                            [0,0,0,0.444,0,0],
-                            [0,0,0,0,0.444,0],
-                            [0,0,0,0,0,0.444]])
- 
     
+    #---------- Methods ---------------
     def rot2eul(self,rot_matrix, seq='XYZ'):
         """
         Convert rotation matrix to Euler angles.
@@ -91,48 +47,24 @@ class TrajectoryGenerator():
         eul = rotation.as_euler(seq, degrees=False)
         return eul
     
-    def eul2angular_vel(self,euler):
-        alpha, beta, gamma, alpha_dot, beta_dot, gamma_dot = euler
-
-        ca, sa = np.cos(alpha), np.sin(alpha)
-        cb, sb = np.cos(beta), np.sin(beta)
-        cg, sg = np.cos(gamma), np.sin(gamma)
-
-        omega_x = ((ca * sg + cg * sa * sb) * 
-                   (ca * sg * alpha_dot + cg * sa * gamma_dot - ca * cb * cg * beta_dot + 
-                    cg * sa * sb * alpha_dot + ca * sb * sg * gamma_dot) +(ca * cg - sa * sb * sg) * 
-                    (ca * cg * alpha_dot - sa * sg * gamma_dot + ca * cb * sg * beta_dot +
-                    ca * cg * sb * gamma_dot - sa * sb * sg * alpha_dot) +
-                    cb * sa * (cb * sa * alpha_dot + ca * sb * beta_dot))
-
-        omega_y = ca * beta_dot - cb * sa * gamma_dot
-        omega_z = sa * beta_dot + ca * cb * gamma_dot
-
-        return np.array([omega_x, omega_y, omega_z])
-    
     def compute_differentiation(self, prev, curr, dt):
         """
         Compute linear velocity given previous pose, current pose, and time increment.
-
         Args:
             prev (np.array): Previous position (x, y, z)
             curr (np.array): Current position (x, y, z)
             dt (float): Time increment (should be > 0)
-
         Returns:
             np.array: Linear velocity (vx, vy, vz)
         """
         if dt <= 0:
             raise ValueError("Time increment must be positive.")
-    
         velocity = (curr - prev) / dt
         return velocity
     
-    ###################################################################
     def compute_jacobian(self, theta, dh_l):
         theta = np.array(theta)
         l1,l2,l3,l4,l5 = dh_l[0], dh_l[1], dh_l[2], dh_l[3], dh_l[4]
-        
         t1, t2, t3, t4, t5, t6 = theta[0] + np.pi / 2, theta[1], theta[2], theta[3], theta[4], theta[5]
       
         sin_t1 = np.sin(t1)
@@ -148,74 +80,37 @@ class TrajectoryGenerator():
         sin_t6 = np.sin(t6)
         cos_t6 = np.cos(t6)
 
+        ## row1
         expr1 = sin_t1
         expr2 = -cos_t1 * sin_t2
         expr3 = cos_t3 * sin_t1 + cos_t2 * cos_t1 * sin_t3
-        expr4 = (
-            sin_t4 * (sin_t3 * sin_t1 - cos_t2 * cos_t3 * cos_t1)
-            - cos_t4 * cos_t1 * sin_t2
-        )
-        expr5 = (
-            cos_t5 * (cos_t3 * sin_t1 + cos_t2 * cos_t1 * sin_t3)
-            - sin_t5 * (
-                cos_t4 * (sin_t3 * sin_t1 - cos_t2 * cos_t3 * cos_t1)
-                + cos_t1 * sin_t2 * sin_t4
-            )
-        )
-        expr6 = (
-            cos_t6 * (
-                sin_t4 * (sin_t3 * sin_t1 - cos_t2 * cos_t3 * cos_t1)
-                - cos_t4 * cos_t1 * sin_t2
-            )
-            + sin_t6 * (
-                cos_t5 * (
-                    cos_t4 * (sin_t3 * sin_t1 - cos_t2 * cos_t3 * cos_t1)
-                    + cos_t1 * sin_t2 * sin_t4
-                )
-                + sin_t5 * (cos_t3 * sin_t1 + cos_t2 * cos_t1 * sin_t3)
-            )
-        )
-
-        ####row 2
+        expr4 = (sin_t4 * (sin_t3 * sin_t1 - cos_t2 * cos_t3 * cos_t1)- cos_t4 * cos_t1 * sin_t2)
+        expr5 = (cos_t5 * (cos_t3 * sin_t1 + cos_t2 * cos_t1 * sin_t3)- sin_t5 * (
+                cos_t4 * (sin_t3 * sin_t1 - cos_t2 * cos_t3 * cos_t1)+ cos_t1 * sin_t2 * sin_t4))
+        expr6 = (cos_t6 * (sin_t4 * (sin_t3 * sin_t1 - cos_t2 * cos_t3 * cos_t1)- cos_t4 * cos_t1 * sin_t2)
+                + sin_t6 * (cos_t5 * (cos_t4 * (sin_t3 * sin_t1 - cos_t2 * cos_t3 * cos_t1)+ cos_t1 * sin_t2 * sin_t4)
+                + sin_t5 * (cos_t3 * sin_t1 + cos_t2 * cos_t1 * sin_t3)))
+        ## row 2
         expr21 = -1
         expr22 = 0
         expr23 = -cos_t2
         expr24 = -sin_t2 * sin_t3
         expr25 = cos_t3 * sin_t2 * sin_t4 - cos_t2 * cos_t4
         expr26 = -sin_t5 * (cos_t2 * sin_t4 + cos_t3 * cos_t4 * sin_t2) - cos_t5 * sin_t2 * sin_t3
-        expr27 = (
-            sin_t6 * (cos_t5 * (cos_t2 * sin_t4 + cos_t3 * cos_t4 * sin_t2) - sin_t2 * sin_t3 * sin_t5)
-            - cos_t6 * (cos_t2 * cos_t4 - cos_t3 * sin_t2 * sin_t4)
-        )
-
-        #### row3
+        expr27 = (sin_t6 * (cos_t5 * (cos_t2 * sin_t4 + cos_t3 * cos_t4 * sin_t2) - sin_t2 * sin_t3 * sin_t5)
+                - cos_t6 * (cos_t2 * cos_t4 - cos_t3 * sin_t2 * sin_t4))
+        ## row3
         expr31 = 0
         expr32 = -cos_t1
         expr33 = -sin_t2 * sin_t1
         expr34 = cos_t2 * sin_t3 * sin_t1 - cos_t3 * cos_t1
         expr35 = -sin_t4 * (cos_t1 * sin_t3 + cos_t2 * cos_t3 * sin_t1) - cos_t4 * sin_t2 * sin_t1
-        expr36 = (
-            sin_t5 * (
-                cos_t4 * (cos_t1 * sin_t3 + cos_t2 * cos_t3 * sin_t1)
-                - sin_t2 * sin_t4 * sin_t1
-            )
-            - cos_t5 * (cos_t3 * cos_t1 - cos_t2 * sin_t3 * sin_t1)
-        )
-        expr37 = (
-            -sin_t6 * (
-                cos_t5 * (
-                    cos_t4 * (cos_t1 * sin_t3 + cos_t2 * cos_t3 * sin_t1)
-                    - sin_t2 * sin_t4 * sin_t1
-                )
-                + sin_t5 * (cos_t3 * cos_t1 - cos_t2 * sin_t3 * sin_t1)
-            )
-            - cos_t6 * (
-                sin_t4 * (cos_t1 * sin_t3 + cos_t2 * cos_t3 * sin_t1)
-                + cos_t4 * sin_t2 * sin_t1
-            )
-        )
-
-        #### row4
+        expr36 = (sin_t5 * (cos_t4 * (cos_t1 * sin_t3 + cos_t2 * cos_t3 * sin_t1)- sin_t2 * sin_t4 * sin_t1)
+                - cos_t5 * (cos_t3 * cos_t1 - cos_t2 * sin_t3 * sin_t1))
+        expr37 = (-sin_t6 * (cos_t5 * (cos_t4 * (cos_t1 * sin_t3 + cos_t2 * cos_t3 * sin_t1)- sin_t2 * sin_t4 * sin_t1)
+                + sin_t5 * (cos_t3 * cos_t1 - cos_t2 * sin_t3 * sin_t1))- cos_t6 * (sin_t4 * (cos_t1 * sin_t3 + cos_t2 * cos_t3 * sin_t1)
+                + cos_t4 * sin_t2 * sin_t1))
+        ## row4
         A = cos_t1 * sin_t3 + cos_t2 * cos_t3 * sin_t1
         B = cos_t3 * cos_t1 - cos_t2 * sin_t3 * sin_t1
         C = sin_t4 * A + cos_t4 * sin_t2 * sin_t1
@@ -225,21 +120,10 @@ class TrajectoryGenerator():
         G = sin_t4 * F - cos_t4 * cos_t1 * sin_t2
         H = cos_t4 * F + cos_t1 * sin_t2 * sin_t4
         expr41 = l4 * C + l5 * (sin_t6 * (cos_t5 * D + sin_t5 * B) + cos_t6 * C) + l3 * sin_t2 * sin_t1
-        expr42 = l5 * (
-            sin_t6 * (
-                cos_t5 * (cos_t2 * cos_t1 * sin_t4 + cos_t3 * cos_t4 * cos_t1 * sin_t2)
-                - cos_t1 * sin_t2 * sin_t3 * sin_t5
-            )
-            - cos_t6 * (cos_t2 * cos_t4 * cos_t1 - cos_t3 * cos_t1 * sin_t2 * sin_t4)
-        ) - l4 * (cos_t2 * cos_t4 * cos_t1 - cos_t3 * cos_t1 * sin_t2 * sin_t4) - l3 * cos_t2 * cos_t1
-
-        expr43 = l4 * sin_t4 * E - l5 * (
-            sin_t6 * (
-                sin_t5 * (sin_t3 * sin_t1 - cos_t2 * cos_t3 * cos_t1)
-                - cos_t4 * cos_t5 * E
-            )
-            - cos_t6 * sin_t4 * E
-        )
+        expr42 = l5 * (sin_t6 * (cos_t5 * (cos_t2 * cos_t1 * sin_t4 + cos_t3 * cos_t4 * cos_t1 * sin_t2)- cos_t1 * sin_t2 * sin_t3 * sin_t5)- 
+                       cos_t6 * (cos_t2 * cos_t4 * cos_t1 - cos_t3 * cos_t1 * sin_t2 * sin_t4))-l4 * (cos_t2 * cos_t4 * cos_t1 - cos_t3 * cos_t1 * 
+                                                                                                      sin_t2 * sin_t4) - l3 * cos_t2 * cos_t1
+        expr43 = l4 * sin_t4 * E - l5 * (sin_t6 * (sin_t5 * (sin_t3 * sin_t1 - cos_t2 * cos_t3 * cos_t1)- cos_t4 * cos_t5 * E)- cos_t6 * sin_t4 * E)
 
         expr44 = l5 * (
             cos_t6 * (cos_t4 * F + cos_t1 * sin_t2 * sin_t4)
@@ -446,7 +330,6 @@ class TrajectoryGenerator():
         Input: theta - 6x1 joint angles
         Output: out - [euler angles (XYZ); position (x,y,z)]
         """
-        # Modified DH Parameters of UR10 (in meters and radians)
         t1, t2, t3, t4, t5, t6, t7 = np.array([theta[0], theta[1], theta[2], theta[3], theta[4], theta[5], theta[6]])
         l1,l2, l3, l4, l5 = dh_l[0], dh_l[1], dh_l[2], dh_l[3], dh_l[4] #np.array([0.10555,0.176,0.3,0.32,0.2251])
         dh = np.array([
@@ -472,10 +355,10 @@ class TrajectoryGenerator():
         T = T1 @T1d @ T2 @ T3 @ T4 @ T5 @ T6 @T7
         angles = self.rot2eul(T[:3,:3])
         position = T[:3,3]
-        return np.concatenate((angles, position))
+        return np.concatenate((angles, position)), T
 
 
-    def joints_trajectory(self, t, theta, dh_l):
+    def joints_trajectory(self, t, theta, dh_l, T_init, T_final):
         """ Generate smooth trajectory using the exponential matrix method. """
         if t>self.traj_time:
             t = self.traj_time
@@ -484,16 +367,27 @@ class TrajectoryGenerator():
                 self.traj_status = False
             return self.joint_angles_both
 
-        R_init = self.T_init[:3, :3]
-        R_final = self.T_final[:3, :3]
-        position_init = self.T_init[:3, 3]
-        position_final = self.T_final[:3, 3]
+        if t<self.traj_time/2:
+            R_init = T_init[:3, :3]
+            R_final = T_final[:3, :3]
+            position_init = T_init[:3, 3]
+            position_final = T_final[:3, 3]
+            s = 10*(t/(self.traj_time/2))**3-15*(t/(self.traj_time/2))**4+6*(t/(self.traj_time/2))**5
+            position_instant = position_init + s * (position_final - position_init)
+        else:
+            R_init = T_final[:3, :3]
+            R_final = T_final[:3, :3]
+            position_init = T_final[:3, 3]
+            s = 10*((t-(self.traj_time/2))/(self.traj_time/2))**3-15*((t-(self.traj_time/2))/(self.traj_time/2))**4+6*((t-(self.traj_time/2))/(self.traj_time/2))**5
+            a = 0.3 #major
+            b = 0.1 #minor
+            x = position_init[0]
+            y = position_init[1] + a*(np.cos(2*np.pi*s)-1)
+            z = position_init[2] + b*(np.sin(2*np.pi*s)-1)
+            position_instant = np.array([x,y,z])
 
-        s = 10*(t/self.traj_time)**3-15*(t/self.traj_time)**4+6*(t/self.traj_time)**5
-
-        # Compute interpolated rotation using exponential map
         if self.R_prev is None:
-            self.R_prev = R_init
+                self.R_prev = R_init
 
         R_instant = R_init @ expm(logm(R_init.T @ R_final) * s)
 
@@ -501,20 +395,6 @@ class TrajectoryGenerator():
         omega_hat = R_dot @ R_instant.T
         orientation_dot = np.array([omega_hat[2,1], omega_hat[0,2], omega_hat[1,0]])
         self.R_prev = R_instant
-        
-        if self.traj_type =="straight":
-            position_instant = position_init + s * (position_final - position_init)
-        
-        elif self.traj_type == "ellipsoid":
-            a = 0.3 #major
-            b = 0.1 #minor
-            x = position_init[0] + b*np.sin(2*np.pi*s)
-            y = position_init[1]-a + a*np.cos(2*np.pi*s)
-            z = position_init[2]
-            position_instant = np.array([x,y,z])
-        else:
-            logging.warning("Provide valid trajectory typr -> traj_type='straight' or 'ellipsoid'!")
-            return
 
         euler_instant = self.rot2eul(R_instant)
 
@@ -524,13 +404,10 @@ class TrajectoryGenerator():
         
         if self.prev_euler is None:
             self.prev_euler = self.rot2eul(R_init)
-        euler_dot = self.compute_differentiation(self.prev_euler, euler_instant, 1/self.sampling_frequency)
 
-        #orientation_dot = self.eul2angular_vel(np.hstack([euler_instant, euler_dot]))
         self.prev_position, self.prev_euler = position_instant, euler_instant
 
         pose, posedot = np.hstack([euler_instant, position_instant]), np.hstack([orientation_dot, position_dot])
-
         #### ik
         J = self.compute_jacobian(theta, dh_l) # 6x7
         JJT = J.T @ J    ## 7x7
@@ -540,47 +417,38 @@ class TrajectoryGenerator():
         else:
             JJT_inv = np.linalg.pinv(JJT_) @ J.T 
 
-        pose_a = self.FK(theta, dh_l)   # [position, orientation]
+        pose_a, _ = self.FK(theta, dh_l)   # [position, orientation]
         if pose_a is None:
             logging.warning("Either didn't receive joint states or error in FK calculation->pose_actual:None")
             return
+        
         #### Null space condition
         q_dot_constraints = (self.mean_joints_limit[7:14].reshape(7,1)-np.array(theta).reshape(7,1))/self.max_min_dif_sq[7:14].reshape(7,1)
         if self.null_space:
-            joint_states_dot_inst = JJT_inv @ ((np.array(posedot).reshape(6,1)) + self.k_ @ (np.array(pose).reshape(6,1)-np.array(pose_a).reshape(6,1))) + (np.eye(7)-JJT_inv@J)@ q_dot_constraints
+            joint_states_dot_inst = JJT_inv @ (np.array(posedot).reshape(6,1)) + (np.eye(7)-JJT_inv@J)@ q_dot_constraints
         else:
-            joint_states_dot_inst = JJT_inv @ ((np.array(posedot).reshape(6,1)) + self.k_ @ (np.array(pose).reshape(6,1)-np.array(pose_a).reshape(6,1)))
+            joint_states_dot_inst = JJT_inv @ (np.array(posedot).reshape(6,1))
         if self.joint_states_prev is None:
             print("previous pose is none")
             self.joint_states_prev = theta
         joint_states_desired = self.integrate_joint_positions(np.array(joint_states_dot_inst).flatten(), self.joint_states_prev, 1/self.sampling_frequency)
-        # if t>self.traj_time:
-        #     joint_states_desired = theta
-        #left_js_desired = np.array([-joint_states_desired[0], -joint_states_desired[1], -joint_states_desired[2], -joint_states_desired[3], -joint_states_desired[4], -joint_states_desired[5], -joint_states_desired[6]])
         joint_angles_both = np.concatenate((joint_states_desired, joint_states_desired))
         self.joint_states_prev = joint_states_desired
         self.joint_angles_both = joint_angles_both
         if t==0:
             print(f"-------------Executing trajectory-------------------")
-            print(f"Final Transformation: {self.T_final}")
+            print(f"Final Transformation: {T_final}")
         return joint_angles_both
         
-    def get_joints(self, t, theta, dh_l):
-        return self.joints_trajectory(t, theta, dh_l)
+    def get_joints(self, t, theta, dh_l, T_init, T_final):
+        return self.joints_trajectory(t, theta, dh_l, T_init, T_final)
     
 if __name__== "__main__":
     obj = TrajectoryGenerator()
-    print(obj.get_joints(0.09, [0.1 for _ in range(7)], obj.dh_l))
-    #print(obj.FK([-0.08567228792299453, 1.3969787867238597, -1.317937334925575, -1.365683925938372, 0.5836437934997851, 0.22152439148382275, -0.3658784425167152], obj.dh_l)) #9.99
-    
-
-    #print(obj.compute_jacobian([0.001543749,	1.100873737,	-1.608122752,	-1.491940354,	0.622086298,	-0.084254206,	-0.145626394], obj.dh_l)) ##9.81
-    #print(obj.compute_jacobian([0.002194408,	0.00158479,	-0.001932682,	-0.000220277,	-0.001782932,	-0.001366595,	0.001518298], obj.dh_l)) ##7
-
-
-    """traj = np.array(pd.read_csv("/home/cstar/Documents/dual_arm_ws/src/python_pkg/data/Book1.csv"))
-    #print(f"traj: {traj[1][1]}")
-    for i in range(len(traj)):
-        print(f"time: {traj[i]}")
-        #[0,0.002400,0.0,0.00300,0.0,0.002400,0.0]
-        print(obj.get_joints(traj[i][0], traj[i][1:], obj.dh_l)) """
+    _, T_init = obj.FK([-0.085, 1.396, -1.317, -1.365, 0.583, 0.221, -0.365], obj.dh_l)
+    T_final = np.array([[0, -1, 0, 0], 
+                                 [0, 0, -1, -.10555-0.176-0.3-0.32-0.2251], #0.10555,0.176,0.3,0.32,0.2251
+                                 [1, 0, 0, 0], 
+                                 [0, 0, 0, 1]])
+    print(obj.get_joints(0.09, [0.1 for _ in range(7)], obj.dh_l, T_init, T_final))
+   
